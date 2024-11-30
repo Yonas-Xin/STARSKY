a25fb37c5c4df176f948508e85d4e37638f6cab4
Yonas-Xin <850021443@qq.com>
2024-11-30 19:53:25 +0800

    优化了画图的逻辑，代码更简洁

--- graph.py
+++ graph.py
@@ -7,6 +7,8 @@ from onnx.checker import check_model
 from onnxsim import simplify
 import onnxruntime as ort
 
+import skystar
+
 
 class Node:
     def __init__(self, node, generation):
@@ -21,8 +23,7 @@ class Graph:
         self.outputs = []
         self.initializers = []
         self.last_func_id = None
-        self.inputindex=1
-
+        self.inputindex = 1
 
     def add_node(self, node):
         self.nodes.append(node)
@@ -45,16 +46,16 @@ def save_graph(graph, model_name, file_name='Example.onnx', ifsimplify=False):
         outputs=graph.outputs,
         initializer=graph.initializers
     )
-    onnx_model = make_model(graph=_graph, opset_imports=[onnx.helper.make_opsetid("", 15)])#指定版本
+    onnx_model = make_model(graph=_graph, opset_imports=[onnx.helper.make_opsetid("", 15)])  # 指定版本
     check_model(model=onnx_model)
     if ifsimplify:
         model_simplified, check = simplify(onnx_model)
         assert check, "Simplified ONNX model could not be validated"
         onnx.save(model_simplified, file_name)
-        print('model saved and simplified successfully')
+        print('model saved and simplified successfully--Path:{}'.format(file_name))
     else:
         onnx.save_model(onnx_model, file_name)
-        print('model saved successfully')
+        print('model saved successfully--Path:{}'.format(file_name))
 
 
 # ===============================================================
@@ -111,7 +112,13 @@ def _graph_node(f, graph):
 # 生成输入输出的名称,输入输出是core的Function函数里前向传播的输入输出，大多数create函数可用
 # ===============================================================
 def generate_names(f, graph):
-    inputs_name = [input.name + f'_{id(input)}' if input.name is not None else f'mid_{id(input)}' for input in f.inputs]
+    inputs_name = []
+    for input in f.inputs:
+        if input.data is not None:#考虑到卷积层的b可能为None
+            if input.name is not None:
+                inputs_name.append(input.name+f'_{id(input)}')
+            else:
+                inputs_name.append(f'mid_{id(input)}')
     if f.generation == 0:
         inputs_name[0] = f'Input{graph.inputindex}'
         node = make_tensor_value_info(f'Input{graph.inputindex}', TensorProto.FLOAT, list(f.inputs[0].shape))
@@ -128,8 +135,12 @@ def generate_names(f, graph):
 # ===============================================================
 def generate_initializers(f, graph):
     for input in f.inputs:
-        if input.name is not None:
-            initializer=make_tensor(input.name + f'_{id(input)}', TensorProto.FLOAT, list(input.shape), input.data)
+        if input.name is not None and input.data is not None:
+            if input.data.dtype in [np.int32,np.int64]:
+                datatype=TensorProto.INT32
+            else:
+                datatype=TensorProto.FLOAT
+            initializer = make_tensor(input.name + f'_{id(input)}', datatype, list(input.shape), input.data)
             if initializer not in graph.initializers:
                 graph.initializers.append(initializer)
 
@@ -149,6 +160,9 @@ def create_node(f, graph, node_type, **kwargs):
     return Node(node, f.generation)
 
 
+# ===============================================================
+# 创建Function节点
+# ===============================================================
 def create_add_node(f, graph):
     return create_node(f, graph, 'Add')
 
@@ -176,37 +190,21 @@ def create_AveragePool_node(f, graph):
 
 
 def create_dropout_node(f, graph):
-    return create_node(f, graph, 'Dropout', seed=f.ratio, training_mode=f.training, mask=f.mask)
+    return create_node(f, graph, 'Dropout', seed=f.ratio, training_mode=skystar.Get_TrainingMode(), mask=f.mask)
 
 
 def create_conv_node(f, graph):
     '''需要初始化权重'''
-    inputs_name, outputs_name = generate_names(f, graph)
-    node = make_node(
-        'Conv',
-        inputs_name,
-        outputs_name,
-        kernel_shape=[f.inputs[1].shape[2], f.inputs[1].shape[3]],
-        strides=[f.stride, f.stride],
-        pads=[f.pad, f.pad, f.pad, f.pad],
-        name=f'Conv_node_{id(f)}'
-    )
-    return Node(node, f.generation)
-
+    return create_node(f, graph, 'Conv',
+                       kernel_shape=[f.inputs[1].shape[2], f.inputs[1].shape[3]],
+                       strides=[f.stride, f.stride],
+                       pads=[f.pad, f.pad, f.pad, f.pad], )
 
 def create_convtranspose_node(f, graph):
-    inputs_name, outputs_name = generate_names(f, graph)
-    node = make_node(
-        'ConvTranspose',
-        inputs_name[:-1],
-        outputs_name,
-        kernel_shape=[f.inputs[1].shape[2], f.inputs[1].shape[3]],
-        strides=[f.stride, f.stride],
-        pads=[f.pad, f.pad, f.pad, f.pad],
-        output_shape=list(f.outputs[0]().shape),
-        name=f'ConvTranspose_node_{id(f)}'
-    )
-    return Node(node, f.generation)
+    return create_node(f, graph, 'ConvTranspose', kernel_shape=[f.inputs[1].shape[2], f.inputs[1].shape[3]],
+                       strides=[f.stride, f.stride],
+                       pads=[f.pad, f.pad, f.pad, f.pad],
+                       output_shape=list(f.outputs[0]().shape), )
 
 
 def create_softmaxcrossentropyloss_node(f, graph):
@@ -226,35 +224,7 @@ def create_softmaxcrossentropyloss_node(f, graph):
 
 
 def create_batchNormalization_node(f, graph):
-    '''BatchNormalization需要自定义生成initializers
-    需要注意的是，由于一些尚未知的版本问题，为了兼容pytorch模型转化，默认batchnorm的运行模式为测试模式，即training_mode=0
-    经过onnxsimplify之后，该层会与conv层融合，因此需要保证使用simplify时，该层处于测试模式，否则简化的模型输出会与原输出有差距'''
-    inputs_name, outputs_name = generate_names(f, graph)
-    graph.initializers.append(make_tensor(f'scale_{id(f.gamma)}', TensorProto.FLOAT, list(f.gamma.shape), f.gamma))
-    graph.initializers.append(make_tensor(f'B_{id(f.beta)}', TensorProto.FLOAT, list(f.beta.shape), f.beta))
-    graph.initializers.append(
-        make_tensor(f'input_mean_{id(f.test_mean)}', TensorProto.FLOAT, list(f.test_mean.shape), f.test_mean))
-    graph.initializers.append(
-        make_tensor(f'input_var_{id(f.test_var)}', TensorProto.FLOAT, list(f.test_var.shape), f.test_var))
-    inputs_name.append(f'scale_{id(f.gamma)}')
-    inputs_name.append(f'B_{id(f.beta)}')
-    inputs_name.append(f'input_mean_{id(f.test_mean)}')
-    inputs_name.append(f'input_var_{id(f.test_var)}')
-    # if f.training:  # 用于符合onnx的标准training==1的情况，但不必要
-    #     graph.outputs.append(make_tensor_value_info(f'Mean_{f.generation}', TensorProto.FLOAT, list(f.test_mean.shape)))
-    #     graph.outputs.append(make_tensor_value_info(f'Var_{f.generation}', TensorProto.FLOAT, list(f.test_var.shape)))
-    #     outputs_name.append(f'Mean_{f.generation}')
-    #     outputs_name.append(f'Var_{f.generation}')
-    node = make_node(
-        'BatchNormalization',
-        inputs_name,
-        outputs_name,
-        epsilon=1e-5,
-        momentum=f.momentum,
-        training_mode=0,#training_mode=f.training,修改成默认为false
-        name=f'BatchNormalization_node_{id(f)}'
-    )
-    return Node(node, f.generation)
+    return create_node(f, graph, 'BatchNormalization', epsilon=1e-5, momentum=f.momentum, training_mode=0)
 
 
 def create_sigmoid_node(f, graph):
@@ -291,34 +261,12 @@ def create_Reshape_node(f, graph):
         name=f'Reshape_node_{id(f)}',
     )
     return Node(node, f.generation)
-def create_slice_node(f,graph):
-    inputs_name, outputs_name = generate_names(f, graph)
-    inputs_name.append(f'starts_{id(f.starts)}')
-    inputs_name.append(f'ends_{id(f.ends)}')
-    inputs_name.append(f'axis_{id(f.starts)}')
-    inputs_name.append(f'steps_{id(f.steps)}')
-    graph.initializers.append(
-        make_tensor(f'starts_{id(f.starts)}', TensorProto.INT64, [len(f.starts)], f.starts)
-    )
-    graph.initializers.append(
-        make_tensor(f'ends_{id(f.ends)}', TensorProto.INT64, [len(f.ends)], f.ends)
-    )
-    graph.initializers.append(
-        make_tensor(f'axis_{id(f.starts)}', TensorProto.INT64, [len(f.axis)], f.axis)
-    )
-    graph.initializers.append(
-        make_tensor(f'steps_{id(f.steps)}', TensorProto.INT64, [len(f.steps)], f.steps)
-    )
-    node = make_node(
-        'Slice',
-        inputs_name,
-        outputs_name,
-        name=f'Slice_node_{id(f)}',
-    )
-    return Node(node, f.generation)
 
-def create_tanh_node(f,graph):
+def create_slice_node(f, graph):
+    return create_node(f, graph, 'Slice')
+def create_tanh_node(f, graph):
     return create_node(f, graph, 'Tanh')
+
 
 # 使用节点的字典
 function_nodes = {
